f3adc2471d40213595d67369c020f7366f6e8db7
Admin <Admin@DESKTOP-38N1J1R>
2021-01-31 16:42:19 +0200

    Add project files.

---
+++ Models/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Demo_01.Models
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
